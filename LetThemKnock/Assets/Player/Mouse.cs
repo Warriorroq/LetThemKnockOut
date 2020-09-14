59846f63146c45fb38cd98a033a7792fb1de504f
Warriorroq <Warriorroq@gmail.com>
2020-09-14 18:05:59 +0300

    Add camera scripts
    idk

---
+++ LetThemKnock/Assets/Player/Mouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    private bool open = true;
+    void Update()
+    {
+        ChangeLock();
+        ChangeState();
+    }
+    private void ChangeLock()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            open = !open;
+    }
+    private void ChangeState()
+    {
+        if (open)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ LetThemKnock/Assets/Player/PlayerCameraRotation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraRotation : MonoBehaviour
+{
+    public float speedVertical;
+    public float speedHorisontal;
+    private Transform body;
+    private float bodyY;
+    private PlayerParams param;
+    private void Start()
+    {
+        body = transform.parent;
+        param = body.gameObject.GetComponent<PlayerParams>();
+    }
+    void Update()
+    {
+        float y = speedVertical * Input.GetAxis("Mouse X");
+        float x = speedHorisontal * -Input.GetAxis("Mouse Y");
+        CheckSlide(y);
+        if(transform.eulerAngles.x + x < 90 || transform.eulerAngles.x + x > 270)
+            transform.Rotate(x, 0, 0);
+        CheckWallRun();
+
+    }
+    private void CheckWallRun()
+    {
+        if (param.currentState == PlayerParams.state.WallRunLeft)
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z - 15f);
+        else if (param.currentState == PlayerParams.state.WallRunRight)
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + 15f);
+    }
+    void CheckSlide(float y)
+    {
+        
+        if (param.currentState != PlayerParams.state.slide)
+            bodyY = body.transform.eulerAngles.y;
+
+        if (param.currentState == PlayerParams.state.slide && Mathf.Abs(bodyY - (transform.eulerAngles.y + y)) < 50f)
+            transform.Rotate(0, y, 0);
+        else if (param.currentState != PlayerParams.state.slide)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, body.transform.eulerAngles.y, 0);
+            body.transform.Rotate(0, y, 0);
+        }
+    }
+}
