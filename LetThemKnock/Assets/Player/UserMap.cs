7a78b4f32405897dbf1596169f2dc55fda4d3281
Warriorroq <Warriorroq@gmail.com>
2020-09-15 20:48:32 +0300

    Update
    End of this shitty day

--- LetThemKnock/Assets/Player/UserMap.cs
+++ LetThemKnock/Assets/Player/UserMap.cs
@@ -16,18 +16,15 @@ public class UserMap : MonoBehaviour
     private void RaycastBot()
     {
         RaycastHit hit;
-        try
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity);
+        if (hit.collider.gameObject.GetComponent<Bot>())
         {
-            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity);
             hit.collider.gameObject.GetComponent<Bot>().ActivePlayer();
             transform.parent = hit.collider.gameObject.transform;
             transform.localPosition = new Vector3(0, 1, 0);
-            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity);
-            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity);
+            GetComponent<PlayerCameraRotation>().enabled = true;
+            GetComponent<UserMap>().enabled = false;
         }
-        catch
-        {
-
-        }
+        
     }
 }
